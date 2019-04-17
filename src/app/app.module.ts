2f9ebcbddb44aa4d3817ced417180b7616c29fbe
Abhishek Datta <abdatta@iitk.ac.in>
2019-04-17 15:46:40 +0530

    Add angular material library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ChatService } from './services/chat.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { ChatService } from './services/chat.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [ChatService],
   bootstrap: [AppComponent]
